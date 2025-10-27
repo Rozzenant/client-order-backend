cf9fd4031f7b7858c1dda09752b1575a4be5e0af
Rozzenant <dmitry.pe4urckin@yandex.ru>
2025-10-27 15:03:02 +0300

    Created Django project

---
+++ store/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='children')
+
+
+class Nomenclature(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='nomenclatures')
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    nomenclature = models.ForeignKey(Nomenclature, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('order', 'nomenclature')
